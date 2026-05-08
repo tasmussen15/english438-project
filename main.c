b324d402a340dc2eee33860736e54b75ede5b2ed
tasmussen <tomboyo150@gmail.com>
2026-05-08 10:42:40 -0500

    Added .gitignore

--- main.c
+++ main.c
@@ -28,22 +28,22 @@ int main() {
     wait_for_enter(1);
     clear_screen();
 
-    printf("The Story So Far:\n");
+    printf("The Story So Far:\n\n");
     printf(
-        "\nYou play as Emily St. Aubert, a recently orphaned child of a landed rural French\n"
+        "You play as Emily St. Aubert, a recently orphaned child of a landed rural French\n"
         "family. Your guardianship has fallen to your inaffectionate aunt, Madame Cheron.\n"
         "She separates you from your beloved Valancourt, a handsome gentleman of good\n"
         "status, and then marries Montoni, the scheming Italian nobleman. Montoni forces\n"
         "you and your aunt to move to his mysterious castle: the Castle of Udolpho. Here\n"
         "he reveals his true character as a villainous leader of bandits and imprisons\n"
-        "your aunt, ultimately leading to her unfortunate death.\n"
+        "your aunt, ultimately leading to her unfortunate death.\n\n"
     );
 
     printf(
-        "\nWith your aunt deceased, it's only a matter of time before Montoni decides to\n"
+        "With your aunt deceased, it's only a matter of time before Montoni decides to\n"
         "get rid of you, too. That is why tonight, you and your faithful servant,\n"
         "Annette, will brave the wrath of Montoni and the schemes of your kidnapping\n"
-        "suitor Count Morano to attempt to make your escape from the Castle of Udolpho.\n"
+        "suitor Count Morano to attempt to make your escape from the Castle of Udolpho.\n\n"
     );
 
     wait_for_enter(0);
